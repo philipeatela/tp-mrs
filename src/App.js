96e2034f74f9f796b83dce19d5441506d433af03
philipeatela <philipe@natahouse.com>
2019-06-04 17:54:15 -0300

    Added paginated request loop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import logo from './logo.svg';
 import './App.css';
+import { callEndpoint, endpoints } from './services/api';
+
+const AppHeader = styled.header`
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppBody = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  min-height: 100vh;
+`;
+
+const AppFooter = styled.footer`
+`;
 
 function App() {
+  const [repoData, setRepoData] = useState(null);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await callEndpoint(endpoints.getAllRepositories);
+    setRepoData(data);
+  }
+
+  console.log(repoData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader>
+        Trabalho Prático MRS - Relatório de Builds
+      </AppHeader>
+      <AppBody>
+        <p>Segue relatório inicial consolidado de informações de build de um projeto específico:</p>
+        {!repoData && <p>Carregando dados...</p>}
+        {repoData && <p>Carregado</p>}
+      </AppBody>
+      <AppFooter>
+        Por Philipe Pinheiro Atela, 2019
+      </AppFooter>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -9,17 +9,65 @@ const api = create({
     'Authorization': 'Basic ' + authdata,
   }
 })
+const rawApi = create({
+  headers: {
+    'Authorization': 'Basic ' + authdata,
+  }
+});
 api.setBaseURL('https://api.bitbucket.org/2.0');
 const TEAM_ID = '{3c98f9a4-15e8-4013-abe6-b97bbb25671e}'
 
+async function delay(ms) {
+  // return await for better async stack trace support in case of errors.
+  return await new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export const callEndpoint = async (endpoint) => {
+  console.log('Calling initial endpoint.')
   const result = await endpoint()
+  let dataValues = []
+  let data
   if (result.ok) {
-    return result.data;
+    data = result.data
+    dataValues = result.data.values
+    if (!data.pagelen || !data.next) {
+      return dataValues
+    }
+
+    let nextUrl = data.next
+    const pages = Math.ceil(data.size / data.pagelen)
+
+    for (let i = 0; i < pages ;i++) {
+      await delay(2000);
+      console.log(`Attempting to call page ${i + 2} ${nextUrl}`);
+      const response = await rawApi.get(nextUrl);
+      console.log(response);
+      if (response.ok) {
+        console.log('Uptading data array and url...')
+        const newData = response.data.values;
+        dataValues = [...dataValues, ...newData];
+        nextUrl = response.data.next;
+      } else {
+        console.log('Failed to call next endpoint.')
+      }
+    }
+    return dataValues;
   }
   if (!result.ok && result.problem) {
     return 'Failed to call endpoint.'
   }
+}
+
+const parseRepositories = data => {
+  const values = data.values;
+  const parsedData = values.map(({ full_name, created_on, mainbranch, uuid }) => ({
+    name: full_name,
+    created_on,
+    mainbranch,
+    id: uuid,
+  }));
+
+  return parsedData;
 }
 
 export const endpoints = {

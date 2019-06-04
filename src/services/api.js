300144669ff81af6bda4fdcbf255993167cf2e8d
philipeatela <philipe@natahouse.com>
2019-06-04 16:14:41 -0300

    Cleaned up api call script code

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,29 @@
 import { create } from 'apisauce'
 
 const username = 'philipeatela'
-const pw = ''
-// const authdata = window.btoa(username + ':' + pw)
+const pw = '3wkA8zuH2SSY2qH57jk6'
 const authdata = Buffer.from(username + ':' + pw).toString('base64')
 
-// const api = create({
-//   baseUrl: 'https://api.bitbucket.org/2.0',
-//   headers: {
-//     'Authorization': 'Basic ' + authdata,
-//   }
-// })
-
+const api = create({
+  headers: {
+    'Authorization': 'Basic ' + authdata,
+  }
+})
+api.setBaseURL('https://api.bitbucket.org/2.0');
 const TEAM_ID = '{3c98f9a4-15e8-4013-abe6-b97bbb25671e}'
 
-const callEndpoint = async () => {
-  // const result = await api.get(`/repositories/${TEAM_ID}/`)
-  const api = create({
-    headers: {
-      'Authorization': 'Basic ' + authdata,
-    }})
-  api.setBaseURL('https://api.bitbucket.org/2.0');
-  console.log(api.baseUrl);
-  const result = await api.get(`/repositories/{3c98f9a4-15e8-4013-abe6-b97bbb25671e}/`)
-  console.log('Attempting request...')
-  console.log(result)
+export const callEndpoint = async (endpoint) => {
+  const result = await endpoint()
+  if (result.ok) {
+    return result.data;
+  }
+  if (!result.ok && result.problem) {
+    return 'Failed to call endpoint.'
+  }
 }
 
-callEndpoint()
+export const endpoints = {
+  getAllRepositories: () => api.get(`/repositories/${TEAM_ID}/`),
+}
+
+callEndpoint(endpoints.getAllRepositories)
